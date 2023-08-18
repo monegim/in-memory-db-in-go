0b3fb3b9661c819e363ec39d105a2d09f8754419
Mostafa Negim <mostafa.negim@gmail.com>
2023-08-18 14:38:46 +0330

    Add: TestDatabase

---
+++ db_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+const (
+	ColumnInt = 1 << iota
+	ColumnVarchar
+)
+
+func TestDatabase(t *testing.T) {
+	db := NewDatabase()
+	col1 := Column{
+		Name: "id",
+		Type: ColumnInt,
+	}
+	col2 := Column{"name", ColumnVarchar}
+	col3 := Column{"population", ColumnInt}
+	db.CreateTable("cities", []Column{
+		col1,
+		col2,
+		col3,
+	})
+	if diff := cmp.Diff(len(db.tables), 1); diff != "" {
+		t.Errorf("Select: (-want +got)\n%s", diff)
+	}
+}
